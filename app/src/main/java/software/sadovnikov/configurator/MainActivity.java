5ba46a18281c50241e98234d90154aab1ac4659e
Sadovnikov Alexey <sadovnikov.russia@gmail.com>
2018-03-20 22:38:40 +0300

    Добавлено меню

--- app/src/main/java/software/sadovnikov/configurator/MainActivity.java
+++ app/src/main/java/software/sadovnikov/configurator/MainActivity.java
@@ -2,13 +2,86 @@ package software.sadovnikov.configurator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+    TextView menu;
+    CheckBox checkBox;
+    public static final String TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "Поиск элементов");
+        tvOut = findViewById(R.id.tvOut);
+        btnOk = findViewById(R.id.btnOk);
+        menu = findViewById(R.id.textView);
+        checkBox = findViewById(R.id.checkBox);
+        btnCancel = findViewById(R.id.btnCancel);
+        btnOk.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    int i = 6 / 0;
+                    tvOut.setText("Результат = " + i);
+                } catch (Exception e) {
+                    Log.d(TAG, "на 0 не делим");
+                }
+
+            }
+        });
+        btnCancel.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvOut.setText(R.string.Нажата_Cancel);
+
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // обновление меню
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
+        // пункты меню с ID группы = 1 видны, если в CheckBox стоит галка
+        menu.setGroupVisible(1, checkBox.isChecked());
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        String sb = "Item Menu" +
+                "\r\n groupId: " + String.valueOf(item.getGroupId()) +
+                "\r\n itemId: " + String.valueOf(item.getItemId()) +
+                "\r\n order: " + String.valueOf(item.getOrder()) +
+                "\r\n title: " + item.getTitle();
+
+        // Выведем в TextView информацию о нажатом пункте меню
+        menu.setText(sb);
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
